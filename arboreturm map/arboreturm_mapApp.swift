b9b5991e1d4d68e1b882e2af763a56ce9d31a106
“danmorgandesigns <dan.morgan@servitech.com>
2025-09-30 10:57:44 -0500

    Initial Commit

---
+++ arboreturm map/arboreturm_mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  arboreturm_mapApp.swift
+//  arboreturm map
+//
+//  Created by Dan Morgan on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct arboreturm_mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
